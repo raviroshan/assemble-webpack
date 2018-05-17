c8bb266f7ab502af826d6c474cfac210eaefd375
Roshan, Ravi (US - Bengaluru) <raroshan@deloitte.com>
2018-05-18 00:06:50 +0530

    Updated indentation of code and readme file.

--- README.md
+++ README.md
@@ -4,38 +4,38 @@ Webpack Loader + Plugin for compiling Handlebars using Assemble.io
 
 # Install
 
-    npm install --save-dev assemble-webpack
+  npm install --save-dev assemble-webpack
 
 # Usage
 
-    const assembleWebpack = require('assemble-webpack');
+  const assembleWebpack = require('assemble-webpack');
 
-    module.exports = {
-      module: {
-        rules: [
-          {
-            test: /\.(hbs)$/,
-            use: [
-    			{
-    				loader: 'assemble-webpack'
-    			}
-    		]
-          }
-        ]
-      },
-      plugins: [
-        new assembleWebpack.AttachedPlugin({
-    		baseLayout: './web/webroot/WEB-INF/layouts/base.hbs',
-    		basePages: ['./web/webroot/WEB-INF/pages/**/*.hbs'],
-    		partialsLayout: ['./web/webroot/WEB-INF/fe-components/**/*.hbs'],
-    		partialsData: [
-    			'./web/webroot/WEB-INF/fe-components/**/*.json',
-    			'./web/webroot/WEB-INF/layouts/**/*.json',
-    			'./web/webroot/WEB-INF/pages/**/*.json'
-    		]
-    	})
+  module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(hbs)$/,
+          use: [
+            {
+              loader: 'assemble-webpack'
+            }
+          ]
+        }
       ]
-    }
+    },
+    plugins: [
+      new assembleWebpack.AttachedPlugin({
+        baseLayout: './web/webroot/WEB-INF/layouts/base.hbs',
+        basePages: ['./web/webroot/WEB-INF/pages/**/*.hbs'],
+        partialsLayout: ['./web/webroot/WEB-INF/fe-components/**/*.hbs'],
+        partialsData: [
+          './web/webroot/WEB-INF/fe-components/**/*.json',
+          './web/webroot/WEB-INF/layouts/**/*.json',
+          './web/webroot/WEB-INF/pages/**/*.json'
+        ]
+      })
+    ]
+  }
 
 You need Handlebars loader so that Webpack can keep an watch on any change in the required hbs files and **assemble-webpack** will generate the new compiled HTML files.
 
--- src/assemble-task.js
+++ src/assemble-task.js
@@ -5,53 +5,45 @@ const extname = require('gulp-extname');
 let app;
 
 function handleAssemble(params) {
-    console.log(chalk.black.bold('Asseble.io - Starts'));
+  console.log(chalk.black.bold('Asseble.io - Starts'));
 
-    // 1. Base Layout
-    const {
-        baseLayout
-    } = params.queries;
-    console.log('baseLayout: ', baseLayout);
+  // 1. Base Layout
+  const { baseLayout } = params.queries;
+  console.log('baseLayout: ', baseLayout);
 
-    // 2. Partials - look for partials from different locations
-    const {
-        partialsLayout
-    } = params.queries;
-    console.log('partialsLayout: ', partialsLayout);
+  // 2. Partials - look for partials from different locations
+  const { partialsLayout } = params.queries;
+  console.log('partialsLayout: ', partialsLayout);
 
-    // 3. Data that will be bound to Partials during compile time
-    // const partialsData = getAbsolutePathArray(params.queries.partialsData);
-    const {
-        partialsData
-    } = params.queries;
-    console.log('partialsData: ', partialsData);
+  // 3. Data that will be bound to Partials during compile time
+  // const partialsData = getAbsolutePathArray(params.queries.partialsData);
+  const { partialsData } = params.queries;
+  console.log('partialsData: ', partialsData);
 
-    // 4. Pages
-    const {
-        basePages
-    } = params.queries;
-    console.log('basePages: ', basePages);
+  // 4. Pages
+  const { basePages } = params.queries;
+  console.log('basePages: ', basePages);
 
-    // 5. Output Folder
-    const outputPath = params.webpackConfig.options.output.path;
+  // 5. Output Folder
+  const outputPath = params.webpackConfig.options.output.path;
 
-    app = assemble();
+  app = assemble();
 
-    app.layouts(baseLayout);
-    app.partials(partialsLayout);
-    app.pages(basePages);
-    app.data(partialsData);
+  app.layouts(baseLayout);
+  app.partials(partialsLayout);
+  app.pages(basePages);
+  app.data(partialsData);
 
-    // 6. Final : Compilation and generating output
-    app
-        .toStream('pages')
-        .pipe(extname())
-        .pipe(app.renderFile())
-        .pipe(app.dest(outputPath));
+  // 6. Final : Compilation and generating output
+  app
+    .toStream('pages')
+    .pipe(extname())
+    .pipe(app.renderFile())
+    .pipe(app.dest(outputPath));
 
-    console.log(chalk.black.bold('Asseble.io - Ends'));
+  console.log(chalk.black.bold('Asseble.io - Ends'));
 }
 
 module.exports = {
-    handleAssemble
+  handleAssemble
 };
