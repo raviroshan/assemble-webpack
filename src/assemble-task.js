511d938fff9596b21a8308047832998605f8a657
Arunkumar Thoutireddy <athoutireddy@deloitte.com>
2018-05-17 15:28:12 +0530

    updated to assemble@0.24.3

--- src/assemble-task.js
+++ src/assemble-task.js
@@ -2,24 +2,23 @@ const chalk = require('chalk');
 const assemble = require('assemble');
 const extname = require('gulp-extname');
 
+let app;
+
 function handleAssemble(params) {
 	console.log(chalk.black.bold('Asseble.io - Starts'));
 
 	// 1. Base Layout
 	const { baseLayout } = params.queries;
 	console.log('baseLayout: ', baseLayout);
-	assemble.layouts(baseLayout);
 
 	// 2. Partials - look for partials from different locations
 	const { partialsLayout } = params.queries;
-	// console.log('partialsLayout: ', partialsLayout);
-	assemble.partials(partialsLayout);
+	console.log('partialsLayout: ', partialsLayout);
 
 	// 3. Data that will be bound to Partials during compile time
 	// const partialsData = getAbsolutePathArray(params.queries.partialsData);
 	const { partialsData } = params.queries;
 	console.log('partialsData: ', partialsData);
-	assemble.data(partialsData);
 
 	// 4. Pages
 	const { basePages } = params.queries;
@@ -28,11 +27,19 @@ function handleAssemble(params) {
 	// 5. Output Folder
 	const outputPath = params.webpackConfig.options.output.path;
 
+    app = assemble({
+        options: {
+            layouts: baseLayout,
+            partials: partialsLayout,
+            data: partialsData
+        }
+    });
+
 	// 6. Final : Compilation and generating output
-	assemble
+	app
 		.src(basePages)
 		.pipe(extname())
-		.pipe(assemble.dest(outputPath));
+		.pipe(app.dest(outputPath));
 
 	console.log(chalk.black.bold('Asseble.io - Ends'));
 }
