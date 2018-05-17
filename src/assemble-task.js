631b73acc007b8c4bb6a39fbf3d2abdeb7f1ec8a
Arunkumar Thoutireddy <athoutireddy@deloitte.com>
2018-05-17 17:36:05 +0530

    beautify

--- src/assemble-task.js
+++ src/assemble-task.js
@@ -5,27 +5,35 @@ const extname = require('gulp-extname');
 let app;
 
 function handleAssemble(params) {
-	console.log(chalk.black.bold('Asseble.io - Starts'));
+    console.log(chalk.black.bold('Asseble.io - Starts'));
 
-	// 1. Base Layout
-	const { baseLayout } = params.queries;
-	console.log('baseLayout: ', baseLayout);
+    // 1. Base Layout
+    const {
+        baseLayout
+    } = params.queries;
+    console.log('baseLayout: ', baseLayout);
 
-	// 2. Partials - look for partials from different locations
-	const { partialsLayout } = params.queries;
-	console.log('partialsLayout: ', partialsLayout);
+    // 2. Partials - look for partials from different locations
+    const {
+        partialsLayout
+    } = params.queries;
+    console.log('partialsLayout: ', partialsLayout);
 
-	// 3. Data that will be bound to Partials during compile time
-	// const partialsData = getAbsolutePathArray(params.queries.partialsData);
-	const { partialsData } = params.queries;
-	console.log('partialsData: ', partialsData);
+    // 3. Data that will be bound to Partials during compile time
+    // const partialsData = getAbsolutePathArray(params.queries.partialsData);
+    const {
+        partialsData
+    } = params.queries;
+    console.log('partialsData: ', partialsData);
 
-	// 4. Pages
-	const { basePages } = params.queries;
-	console.log('basePages: ', basePages);
+    // 4. Pages
+    const {
+        basePages
+    } = params.queries;
+    console.log('basePages: ', basePages);
 
-	// 5. Output Folder
-	const outputPath = params.webpackConfig.options.output.path;
+    // 5. Output Folder
+    const outputPath = params.webpackConfig.options.output.path;
 
     app = assemble();
 
@@ -34,16 +42,16 @@ function handleAssemble(params) {
     app.pages(basePages);
     app.data(partialsData);
 
-	// 6. Final : Compilation and generating output
-	app
-		.toStream('pages')
-		.pipe(extname())
+    // 6. Final : Compilation and generating output
+    app
+        .toStream('pages')
+        .pipe(extname())
         .pipe(app.renderFile())
-		.pipe(app.dest(outputPath));
+        .pipe(app.dest(outputPath));
 
-	console.log(chalk.black.bold('Asseble.io - Ends'));
+    console.log(chalk.black.bold('Asseble.io - Ends'));
 }
 
 module.exports = {
-	handleAssemble
+    handleAssemble
 };
